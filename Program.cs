b7edec5d249b8f70d73ba9e5aa6108bba04d6b41
miguel <miguelpositivo02@gmail.com>
2023-04-18 20:57:41 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string frase, resoluçao;
+Console.WriteLine("digite o que voce acha sobre mim?");
+frase = Console.ReadLine()!;
+resoluçao = $"{frase.Remove(10)}... NÃO, PERA \n to nervoso ";
+Console.WriteLine(resoluçao);
